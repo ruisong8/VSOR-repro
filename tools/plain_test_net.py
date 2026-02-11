f9761629119be1f851577eae8afb9247b14b252e
ruisong8 <145747699+ruisong8@users.noreply.github.com>
2026-02-11 19:53:47 +0800

    Update plain_test_net.py

--- tools/plain_test_net.py
+++ tools/plain_test_net.py
@@ -25,7 +25,7 @@ path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, path)
 
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "8"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import logging
 
